177014cd4a73e3fe2477dc76e84e2607ab6c4c31
IceMimosa <chk@terminus.io>
2018-03-11 22:05:37 +0800

    Add netty demo

--- rpc-test/src/main/java/io/patamon/test/tcp/netty/TestClient.kt
+++ rpc-test/src/main/java/io/patamon/test/tcp/netty/TestClient.kt
@@ -1,5 +1,18 @@
 package io.patamon.test.tcp.netty
 
+import io.netty.bootstrap.Bootstrap
+import io.netty.buffer.ByteBuf
+import io.netty.buffer.Unpooled
+import io.netty.channel.ChannelHandlerContext
+import io.netty.channel.ChannelInboundHandlerAdapter
+import io.netty.channel.ChannelInitializer
+import io.netty.channel.ChannelOption
+import io.netty.channel.nio.NioEventLoopGroup
+import io.netty.channel.socket.SocketChannel
+import io.netty.channel.socket.nio.NioSocketChannel
+import java.net.InetSocketAddress
+import java.nio.charset.Charset
+
 /**
  * Desc: netty client
  *
@@ -11,7 +24,64 @@ object TestClient {
 
     @JvmStatic
     fun main(args: Array<String>) {
+        val group = NioEventLoopGroup()
+        val bootstrap = Bootstrap()
+        bootstrap.group(group)
+                .channel(NioSocketChannel::class.java)
+                .option(ChannelOption.TCP_NODELAY, true)
+                .handler(object : ChannelInitializer<SocketChannel>() {
+                    override fun initChannel(ch: SocketChannel) {
+                        val pipeline = ch.pipeline()
+                        // 添加pipeline
+                        pipeline.addLast("handler", ClientChannelHandler())
+                    }
+                })
+        val future = bootstrap.connect(InetSocketAddress("localhost", 10080)).sync()
+        future.channel().closeFuture().sync()
 
+        // 关闭group
+        group.shutdownGracefully()
     }
 
+}
+
+class ClientChannelHandler : ChannelInboundHandlerAdapter() {
+    /**
+     * Calls [ChannelHandlerContext.fireChannelRead] to forward
+     * to the next [ChannelInboundHandler] in the [ChannelPipeline].
+     *
+     * Sub-classes may override this method to change behavior.
+     */
+    override fun channelRead(ctx: ChannelHandlerContext, msg: Any) {
+        // super.channelRead(ctx, msg)
+        val buf = msg as ByteBuf
+        println("From Server Channel msg: ${buf.readCharSequence(buf.readableBytes(), Charset.defaultCharset())}")
+        ctx.close()
+    }
+
+    /**
+     * Calls [ChannelHandlerContext.fireChannelActive] to forward
+     * to the next [ChannelInboundHandler] in the [ChannelPipeline].
+     *
+     * Sub-classes may override this method to change behavior.
+     */
+    override fun channelActive(ctx: ChannelHandlerContext) {
+        // super.channelActive(ctx)
+        println("Client Channel in active...")
+
+        val buffer = Unpooled.buffer(4096)
+        buffer.writeBytes("Hello Server !!!".toByteArray())
+        ctx.writeAndFlush(buffer)
+    }
+
+    /**
+     * Calls [ChannelHandlerContext.fireExceptionCaught] to forward
+     * to the next [ChannelHandler] in the [ChannelPipeline].
+     *
+     * Sub-classes may override this method to change behavior.
+     */
+    override fun exceptionCaught(ctx: ChannelHandlerContext?, cause: Throwable?) {
+        // super.exceptionCaught(ctx, cause)
+        println("Client Channel in exception caught...")
+    }
 }
--- rpc-test/src/main/java/io/patamon/test/tcp/netty/TestServer.kt
+++ rpc-test/src/main/java/io/patamon/test/tcp/netty/TestServer.kt
@@ -1,12 +1,18 @@
 package io.patamon.test.tcp.netty
 
 import io.netty.bootstrap.ServerBootstrap
+import io.netty.buffer.ByteBuf
+import io.netty.buffer.Unpooled
 import io.netty.channel.Channel
+import io.netty.channel.ChannelHandlerContext
+import io.netty.channel.ChannelInboundHandlerAdapter
 import io.netty.channel.ChannelInitializer
 import io.netty.channel.ChannelOption
 import io.netty.channel.nio.NioEventLoopGroup
 import io.netty.channel.socket.nio.NioServerSocketChannel
+import io.netty.handler.codec.string.StringDecoder
 import java.net.InetSocketAddress
+import java.nio.charset.Charset
 
 /**
  * Desc: netty server
@@ -20,8 +26,8 @@ object TestServer {
     @JvmStatic
     fun main(args: Array<String>) {
 
-        val bossGroup = NioEventLoopGroup()
-        val workGroup = NioEventLoopGroup()
+        val bossGroup = NioEventLoopGroup(Runtime.getRuntime().availableProcessors() * 2)
+        val workGroup = NioEventLoopGroup(5)
 
         val bootstrap = ServerBootstrap()
         bootstrap.group(bossGroup, workGroup)
@@ -30,10 +36,58 @@ object TestServer {
                 .childHandler(object : ChannelInitializer<Channel>() {
                     override fun initChannel(ch: Channel) {
                         val pipeline = ch.pipeline()
-
+                        // 添加pipeline
+                        pipeline.addLast(StringDecoder(Charset.defaultCharset()))
+                        pipeline.addLast(StringDecoder(Charset.defaultCharset()))
+                        pipeline.addLast(ServerChannelHandler())
                     }
                 })
-        val future = bootstrap.bind(InetSocketAddress("", 10080)).sync()
+        val future = bootstrap.bind(InetSocketAddress("localhost", 10080)).sync()
         future.channel().closeFuture().sync()
+
+        // bossGroup.shutdownGracefully()
+        // workGroup.shutdownGracefully()
+    }
+}
+
+class ServerChannelHandler : ChannelInboundHandlerAdapter() {
+
+    /**
+     * Calls [ChannelHandlerContext.fireChannelRead] to forward
+     * to the next [ChannelInboundHandler] in the [ChannelPipeline].
+     *
+     * Sub-classes may override this method to change behavior.
+     */
+    override fun channelRead(ctx: ChannelHandlerContext, msg: Any) {
+        // super.channelRead(ctx, msg)
+        val buf = msg as ByteBuf
+        println("Server Channel msg: ${buf.readCharSequence(buf.readableBytes(), Charset.defaultCharset())}")
+
+        // 写
+        val buffer = Unpooled.buffer(4096)
+        buffer.writeBytes("Hello client...".toByteArray())
+        ctx.writeAndFlush(buffer)
+    }
+
+    /**
+     * Calls [ChannelHandlerContext.fireChannelActive] to forward
+     * to the next [ChannelInboundHandler] in the [ChannelPipeline].
+     *
+     * Sub-classes may override this method to change behavior.
+     */
+    override fun channelActive(ctx: ChannelHandlerContext?) {
+        super.channelActive(ctx)
+        println("Server Channel in active...")
+    }
+
+    /**
+     * Calls [ChannelHandlerContext.fireExceptionCaught] to forward
+     * to the next [ChannelHandler] in the [ChannelPipeline].
+     *
+     * Sub-classes may override this method to change behavior.
+     */
+    override fun exceptionCaught(ctx: ChannelHandlerContext?, cause: Throwable?) {
+        // super.exceptionCaught(ctx, cause)
+        println("Server Channel in exception caught...$cause")
     }
 }
